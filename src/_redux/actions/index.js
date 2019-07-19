146ea685306dcd4d85f0b5bc2672b354f71c509e
Kevin Krato <kratok08@gmail.com>
2019-07-19 10:18:28 -0600

    Correct action pathing

--- src/_redux/actions/index.js
+++ src/_redux/actions/index.js
@@ -1,4 +1,4 @@
-export const randomNumber = number => {
+export const setRandomNumber = number => {
   return {
     type: "NG_SET_RANDOM_NUMBER",
     number
--- src/_redux/reducers/allReducers.js
+++ src/_redux/reducers/allReducers.js
@@ -1,7 +1,7 @@
 export const NGRandomNumber = (state = null, action) => {
   switch (action.type) {
     case "NG_SET_RANDOM_NUMBER":
-      return action.payload;
+      return action.number;
     default:
       return state;
   }
