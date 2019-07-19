9ebf57758b6c04f28d50c4f0f401d494d6f15a18
Kevin Krato <kratok08@gmail.com>
2019-07-19 09:27:17 -0600

    Create generic customizable TextInput

---
+++ src/applications/NumberGuesser/components/Input/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Input.scss";
+
+export default function TextInput(props) {
+  return (
+    <article className="TextInput" style={{ width: `${props.width}` }}>
+      <label htmlFor={`${props.name}`}>{props.label}</label>
+      <input
+        type="text"
+        name={`${props.name}`}
+        autoComplete={`${props.autoComplete}`}
+        onChange={props.handleChange}
+      />
+    </article>
+  );
+}
