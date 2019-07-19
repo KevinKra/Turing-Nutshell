003ed7911d8cf08eb058de0e37777decb78aa2a9
Kevin Krato <kratok08@gmail.com>
2019-07-19 17:32:52 -0600

    Integrate component with store

--- src/applications/NumberGuesser/containers/LatestScore/LatestScore.js
+++ src/applications/NumberGuesser/containers/LatestScore/LatestScore.js
@@ -9,6 +9,14 @@ function LatestScore(props) {
     challengerTwoGuess,
     challengerOneGuess
   } = props.challengerData;
+
+  const determineProximity = guess => {
+    if (guess > props.NGNumbers.randomNumber) return "That's too high!";
+    if (guess < props.NGNumbers.randomNumber) return "That's too low!";
+    if (guess === props.NGNumbers.randomNumber) return "Winner!";
+    return "(Make a guess!)";
+  };
+
   return (
     <section className="container-results-section container">
       <h3>Latest Score</h3>
@@ -17,13 +25,13 @@ function LatestScore(props) {
           <h4>{challengerOneName || "Challenger 1 Name"}</h4>
           <p>current guess</p>
           <p>{challengerOneGuess || 0}</p>
-          <p>that's too high</p>
+          <p>{determineProximity(challengerOneGuess)}</p>
         </aside>
         <aside className="challenger-output-2">
           <h4>{challengerTwoName || "Challenger 2 Name"}</h4>
           <p>current guess</p>
           <p>{challengerTwoGuess || 0}</p>
-          <p>that's too low</p>
+          <p>{determineProximity(challengerTwoGuess)}</p>
         </aside>
       </div>
     </section>
@@ -31,6 +39,7 @@ function LatestScore(props) {
 }
 
 const mapStateToProps = store => ({
+  NGNumbers: store.NGNumbers,
   challengerData: store.NGChallengerData
 });
 
