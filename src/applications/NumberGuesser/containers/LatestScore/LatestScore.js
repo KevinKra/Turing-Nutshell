559454ff819d27b4c83bdd204dc4d671fc5af684
Kevin Krato <kratok08@gmail.com>
2019-07-19 17:19:27 -0600

    Connect to Store and conditionally render based on status

--- src/applications/NumberGuesser/containers/LatestScore/LatestScore.js
+++ src/applications/NumberGuesser/containers/LatestScore/LatestScore.js
@@ -1,24 +1,37 @@
 import React from "react";
 import "./LatestScore.scss";
+import { connect } from "react-redux";
 
-export default function LatestScore() {
+function LatestScore(props) {
+  const {
+    challengerOneName,
+    challengerTwoName,
+    challengerTwoGuess,
+    challengerOneGuess
+  } = props.challengerData;
   return (
     <section className="container-results-section container">
       <h3>Latest Score</h3>
       <div className="challenger-outputs">
         <aside className="challenger-output-1">
-          <h4>Challenger 1 Name</h4>
+          <h4>{challengerOneName || "Challenger 1 Name"}</h4>
           <p>current guess</p>
-          <p>50</p>
+          <p>{challengerOneGuess || 0}</p>
           <p>that's too high</p>
         </aside>
         <aside className="challenger-output-2">
-          <h4>Challenger 2 Name</h4>
+          <h4>{challengerTwoName || "Challenger 2 Name"}</h4>
           <p>current guess</p>
-          <p>45</p>
+          <p>{challengerTwoGuess || 0}</p>
           <p>that's too low</p>
         </aside>
       </div>
     </section>
   );
 }
+
+const mapStateToProps = store => ({
+  challengerData: store.NGChallengerData
+});
+
+export default connect(mapStateToProps)(LatestScore);
