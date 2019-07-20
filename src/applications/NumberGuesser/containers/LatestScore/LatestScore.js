383b5987fe806de56aa0378e0c447bc8777fa90b
Kevin Krato <kratok08@gmail.com>
2019-07-20 09:42:27 -0600

    Update store with winner data

--- src/applications/NumberGuesser/containers/LatestScore/LatestScore.js
+++ src/applications/NumberGuesser/containers/LatestScore/LatestScore.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LatestScore.scss";
 import { connect } from "react-redux";
+import * as actions from "../../../../_redux/actions";
 
 function LatestScore(props) {
   const {
@@ -10,11 +11,13 @@ function LatestScore(props) {
     challengerOneGuess
   } = props.challengerData;
 
-  const determineProximity = guess => {
+  const determineProximity = (guess, name) => {
     const parsed = parseInt(guess);
     if (parsed > props.storeNumbers.randomNumber) return "That's too high!";
     if (parsed < props.storeNumbers.randomNumber) return "That's too low!";
-    if (parsed === props.storeNumbers.randomNumber) return "Winner!";
+    if (parsed === props.storeNumbers.randomNumber) {
+      props.addNewRound({ challengerOneName, challengerTwoName, winner: name });
+    }
     return "(Make a guess!)";
   };
 
@@ -26,13 +29,13 @@ function LatestScore(props) {
           <h4>{challengerOneName || "Challenger 1 Name"}</h4>
           <p>current guess</p>
           <p>{challengerOneGuess || 0}</p>
-          <p>{determineProximity(challengerOneGuess)}</p>
+          <p>{determineProximity(challengerOneGuess, challengerOneName)}</p>
         </aside>
         <aside className="challenger-output-2">
           <h4>{challengerTwoName || "Challenger 2 Name"}</h4>
           <p>current guess</p>
           <p>{challengerTwoGuess || 0}</p>
-          <p>{determineProximity(challengerTwoGuess)}</p>
+          <p>{determineProximity(challengerTwoGuess, challengerTwoName)}</p>
         </aside>
       </div>
     </section>
@@ -44,4 +47,11 @@ const mapStateToProps = store => ({
   challengerData: store.NGChallengerData
 });
 
-export default connect(mapStateToProps)(LatestScore);
+const mapDispatchToProps = dispatch => ({
+  addNewRound: round => dispatch(actions.addNewRound(round))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LatestScore);
